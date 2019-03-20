cd6689444a0623d2263f13aca24593a03996f269
JeremyWoo <jeremy.woo@c3posttrade.com>
2019-03-21 08:26:23 +1300

    add test client

---
+++ client/client.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/sacOO7/gowebsocket"
+)
+
+func main() {
+
+	var hostname = flag.String("h", "localhost", "help message for flagname")
+
+	flag.Parse()
+
+	fmt.Println("hostname: ", *hostname)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	targetServer := "ws://" + *hostname
+
+	socket := gowebsocket.New(targetServer)
+
+	socket.OnConnected = func(socket gowebsocket.Socket) {
+		log.Println("Connected to server")
+	}
+
+	socket.OnConnectError = func(err error, socket gowebsocket.Socket) {
+		log.Println("Recieved connect error ", err)
+	}
+
+	socket.OnTextMessage = func(message string, socket gowebsocket.Socket) {
+		log.Println("Recieved message " + message)
+	}
+
+	socket.OnBinaryMessage = func(data []byte, socket gowebsocket.Socket) {
+		log.Println("Recieved binary data ", data)
+	}
+
+	socket.OnPingReceived = func(data string, socket gowebsocket.Socket) {
+		log.Println("Recieved ping " + data)
+	}
+
+	socket.OnPongReceived = func(data string, socket gowebsocket.Socket) {
+		log.Println("Recieved pong " + data)
+	}
+
+	socket.OnDisconnected = func(err error, socket gowebsocket.Socket) {
+		log.Println("Disconnected from server ")
+		return
+	}
+
+	socket.Connect()
+
+	for {
+		select {
+		case <-interrupt:
+			log.Println("interrupt")
+			socket.Close()
+			return
+		}
+	}
+}
